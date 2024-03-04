72144b4ff4b02b20fb1f586faceecdc8057f5baf
socet113 <socet113@asicfab.ecn.purdue.edu>
2024-03-04 15:26:28 -0500

    update decode

--- src/rv32zhinx_decode.sv
+++ src/rv32zhinx_decode.sv
@@ -8,19 +8,20 @@ module rv32zhinx_decode (
     rv32zhinx_insn_t insn_split;
 
     assign insn_split = rv32zhinx_insn_t'(insn); 
-
     assign rv32zhinx_control.select = '0;
+    assign rv32zhinx_control.op = '0;
+
     always_comb begin
         casez (fpu_funct_t'(insn_split.opcode))
-            OPCODE_OPFP: rv32zhinx_control.select = 3'd0;
+            OPCODE_OPFP: begin 
+                        rv32zhinx_control.select = 3'd0;
+                        rv32zhinx_control.op = fpu_funct_t'(insn_split.funct5)
+            end
             OPCODE_FMADD: rv32zhinx_control.select = 3'd1;
             OPCODE_FMSUB: rv32zhinx_control.select = 3'd2;
             OPCODE_FNMADD: rv32zhinx_control.select = 3'd3;
             OPCODE_FNMSUB: rv32zhinx_control.select = 3'd4;
         endcase
     end
-    
-    assign rv32zhinx_control.op = fpu_funct_t'(insn_split.funct5);
-
 
 endmodule
