7ea9b43e6d07b807afcb5c63f98cf7bc32c91884
Saandiya <saandiya.mohan@gmail.com>
2024-10-30 18:14:03 -0400

    comments from om

--- src/single_precision/adder.sv
+++ src/single_precision/adder.sv
@@ -65,7 +65,7 @@ module adder(
     assign exp2 = data2[30:23];
 
     assign shift1 = data1[22:0];
-    assign shift2 = data2[22:0]; 
+    assign shift2 = data2[22:0]; //
 
     assign sign1 = data1[31];
     assign sign2 = data2[31];
@@ -78,15 +78,26 @@ module adder(
         result = '0;
         count = '0;
         if(exp1 > exp2) begin
-            exp_sub = exp1 - exp2;
+            exp_sub = exp1 - exp2;  // do exp1 + 2's complement of exp2 (exp1 + (-exp2))
             if(count == 0) begin
-               mant2 = shift2 << 1; //is this right
+               mant2 = shift2 << 1; //is this right // - think it should be right shift, what we have is left shift
+               count = count + 1; // = expsub
+
+               // we already shifted somehting
+
+               //line 81 - do 2's complement
+               //dont use count, and if loop
+               //mant2 = shift 2 >> exp_sub
+            end
+            while(count < exp_sub - 1) begin // check if working first, if not wherever there is -, use 2's complement 
+               mant2 = shift2 << 0; // whatever we did in line 83 got erased //is this correct // while loop doesnt work in verilog & shifting by 0 doesnt work
+               //not synthesizable in verilog, use for loop in verilog
+               //only use while loop in tb
+               //change the while loop
+              // count is not necessary
                count = count + 1;
             end
-            while(count < exp_sub - 1) begin
-               mant2 = shift2 << 0; //is this correct
-               count = count + 1;
-            end
+            //a = b >> 4 - shifting b right by 4 bits
             biggerExp = exp1;
         end
         else begin
@@ -102,7 +113,8 @@ module adder(
             biggerExp = exp2;
         end 
 
-
+// return result as per format according to IEE-754 according to data
+//refer to justin and om discussion, right shifting with 1 in msb, make sure logic is right
 
         case({sign1,sign2}) 
             2'b00: begin result = {1'b0, biggerExp, mant1 + mant2}; end
