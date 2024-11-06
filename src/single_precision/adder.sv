c4f32fc2546f982eba588d93a30244d6a776e489
Saandiya <saandiya.mohan@gmail.com>
2024-11-06 14:25:29 -0500

    updated adder.sv

--- src/single_precision/adder.sv
+++ src/single_precision/adder.sv
@@ -1,68 +1,36 @@
-
-// module fracAdder (
-//     input logic [22:0] data1, data2,
-//     output logic sum[22:0],
-//     output logic carryOut
-// );
-//     logic [23:0] sumTemp;
-    
-//     always_comb begin
-//     sumTemp = data1 + data2;
-//     carryOut = sumTemp[23];
-//     sum = sumTemp[22:0];
-//     end
-
-// endmodule
-
-// module expAdder(
-//     input logic [31:23] data1, data2,
-//     output logic exp[8:0]
-
-// );
-
-// always_comb begin
-//     exp = data1 + data2;
-// end
-
-// endmodule
-
-// module concat(
-//     input logic [31:0] data1, data2,
-//     output logic [31:0] output
-// );
-
-// logic [8:0] exponent;
-// logic carryOut;
-// logic [22:0] sum;
-
-
-// always_comb begin
-//     fracAdder u0 (.data1(data1[22:0]), .data2(data2[22:0]), .sum(sum), .carryOut(carryOut));
-//     expAdder u1 (.data1(data1[31:23]), .data2(data2[31:23]), .exp(exponent));
-//     output = {exponent, sum};
-// end
-
-// endmodule
-
 module adder(
     input logic [31:0] data1, data2,
-    output logic [31:0] result
+    output logic [31:0] result,
+    output logic overflow,
+    output logic underflow
 );
     //making logic variables for the operations
-    logic [7:0] exp_sub;
-    logic sign1;
-    logic sign2;
-    logic [7:0] exp1;
-    logic [7:0] exp2;
-    logic [22:0] mant1;
-    logic [22:0] mant2;
-    logic [7:0] biggerExp;
-    logic [22:0] shift1;
-    logic [22:0] shift2;
-    logic [7:0] count;
-    logic [23:0] carroutCheck;
-    logic [22:0] mantissaResult;
+    //extract sign, exponent, and mantissa from inputs
+    logic sign1; //sign for data 1
+    logic sign2; // sign for data 2
+    logic [7:0] exp1; //exp data 1
+    logic [7:0] exp2; //exp data 2
+    logic [23:0] mant1; //mantissa data 1
+    logic [23:0] mant2; //mantissa data 1
 
+//The mantissa is 23 bits, but the normalized value has an implicit leading 1, 
+//which makes it effectively a 24-bit value in calculations.
+
+    //internal signals
+    logic [7:0] exp_sub; 
+    logic sign_result; //sign
+    logic [24:0] mantissaResult; //mantissa result
+    logic [23:0] shift1; //mant 1 alligned
+    logic [23:0] shift2; //mant 2 aligned
+    //logic [7:0] count;
+    //logic [23:0] carroutCheck;
+    logic [7:0] biggerExp; //exp result
+
+    //normalized signals
+    logic [7:0] normalized_exp;
+    logic [23:0] normalized_mant;
+    logic overflow_temp, underflow_temp;
+    
     //assign exp of data to exp 1 and exp 2
     assign exp1 = data1[30:23];
     assign exp2 = data2[30:23];
@@ -70,66 +38,107 @@ module adder(
     assign sign1 = data1[31];
     assign sign2 = data2[31];
 
+    assign mant1 = {1'b1, data1[22:0]}; // Adding implicit leading 1
+    assign mant2 = {1'b1, data2[22:0]}; // Adding implicit leading 1
+
     //subtract exponents
     always_comb begin
-        mant1 = '0;
-        mant2 = '0;
+        // mant1 = '0;
+        // mant2 = '0;
         result = '0;
-        shift2 = data2[22:0]; //mantissa data for second register
-        shift1 = data1[22:0]; //mantissa data for first register
+        // shift2 = data2[22:0]; //mantissa data for second register
+        // shift1 = data1[22:0]; //mantissa data for first register
         if(exp1 > exp2) begin
             exp_sub = exp1 + (~exp2 + 1);  // do exp1 + 2's complement of exp2 (exp1 + (-exp2))
-            
-            mant2 = {1'b1, shift2[22:1]}; //Shift in 1 to the second mantissa for implied "1.####"
-            mant2 = mant2 >> exp_sub; 
-
+            shift1 = mant1;
+            //mant2 = {1'b1, shift2[22:1]}; //Shift in 1 to the second mantissa for implied "1.####"
+            shift2 = mant2 >> exp_sub; 
             //a = b >> 4 - shifting b right by 4 bits
             biggerExp = exp1;
         end
         else begin
             exp_sub = exp2 + (~exp1 + 1);
-            
-            mant1 = {1'b1, shift1[22:1]}; //Shift in 1 to the first mantissa for implied "1.####"
-            mant1 = shift1 >> exp_sub;
-            
+            shift2 = mant2;
+            //mant1 = {1'b1, shift1[22:1]}; //Shift in 1 to the first mantissa for implied "1.####"
+            shift1 = mant1 >> exp_sub;
             biggerExp = exp2;
         end 
+    end
 
 // return result as per format according to IEE-754 according to data
 //refer to justin and om discussion, right shifting with 1 in msb, make sure logic is right
 
-        carroutCheck = mant1 + mant2; //this checks for a carryout value by using a logic variable that is 24 bits
-        if(carroutCheck[23]) begin//If the 23rd bit is equal to 1, then there is a carry out
+        // carroutCheck = mant1 + mant2; //this checks for a carryout value by using a logic variable that is 24 bits
+        // if(carroutCheck[23]) begin//If the 23rd bit is equal to 1, then there is a carry out
         
-            carroutCheck = carroutCheck >> 1; //The entire 23rd bit value is shifted by 1
-            biggerExp = biggerExp + 1; //Then 1 is added to the exponent
-            mantissaResult = {carroutCheck[22:0]}; //Mantissa result for addition is set to the 23 bits that were shifted in the if statement.
+        //     carroutCheck = carroutCheck >> 1; //The entire 23rd bit value is shifted by 1
+        //     biggerExp = biggerExp + 1; //Then 1 is added to the exponent
+        //     mantissaResult = {carroutCheck[22:0]}; //Mantissa result for addition is set to the 23 bits that were shifted in the if statement.
+        // end
+        // else begin
+        //assign mantissaResult = shift1 + shift2; //Mantissa result for addition is set to the mantissas added if there is no carryout to worry about
+        //end
+
+    //Add or subtract mantissas based on signs
+    always_comb begin
+        if (sign1 == sign2) begin
+            mantissaResult = shift1 + shift2;
+            sign_result = sign1;
+        end else begin
+            if (shift1 > shift2) begin
+                mantissaResult = shift1 - shift2;
+                sign_result = sign1;
+            end else begin
+                mantissaResult = shift2 - shift1;
+                sign_result = sign2;
+            end
         end
-        else begin
-            mantissaResult = mant1 + mant2; //Mantissa result for addition is set to the mantissas added if there is no carryout to worry about
+    end
+
+    always_comb begin
+        if (mant_result[24]) begin
+            // check for carry out, shift right and increment exponent
+            normalized_mant = mantissaResult[24:1];
+            normalized_exp = biggerExp + 1;
+        end else begin
+            normalized_mant = mantissaResult[23:0];
+            normalized_exp = biggerExp;
         end
 
-        case({sign1,sign2}) 
-            2'b00: begin result = {1'b0, biggerExp, mantissaResult}; end
-            2'b11: begin result = {1'b1, biggerExp, mantissaResult}; end
-            2'b01: begin 
-                if(mant1 > mant2) begin
-                    result = {1'b0, biggerExp, mant1 - mant2}; 
-                end
-                else begin
-                    result = {1'b1, biggerExp, mant1 - mant2}; 
-                end
-            end
-            2'b10: begin 
-                if(mant2 > mant1) begin
-                    result = {1'b0, biggerExp, mantissaResult}; 
-                end
-                else begin
-                    result = {1'b1, biggerExp, mantissaResult}; 
-                end
-            end
-            default: result = '0;
-        endcase 
+        // Check for overflow and underflow
+        overflow_temp = (normalized_exp >= 8'hFF);  // Overflow if exponent is maxed out
+        underflow_temp = (normalized_exp == 0);     // Underflow if exponent is zero
     end
+
+    //final result according to IEE-754
+    always_comb begin
+        result = {sign_result, normalized_exp[7:0], normalized_mant[22:0]};
+        overflow = overflow_temp;
+        underflow = underflow_temp;
+    end
+
+
+    //     case({sign1,sign2}) 
+    //         2'b00: begin result = {1'b0, biggerExp, shift1 + shift2}; end
+    //         2'b11: begin result = {1'b1, biggerExp, shift1 + shift2}; end
+    //         2'b01: begin 
+    //             if(mant1 > mant2) begin
+    //                 result = {1'b0, biggerExp, mant1 - mant2}; 
+    //             end
+    //             else begin
+    //                 result = {1'b1, biggerExp, mant1 - mant2}; 
+    //             end
+    //         end
+    //         2'b10: begin 
+    //             if(mant2 > mant1) begin
+    //                 result = {1'b0, biggerExp, mantissaResult}; 
+    //             end
+    //             else begin
+    //                 result = {1'b1, biggerExp, mantissaResult}; 
+    //             end
+    //         end
+    //         default: result = '0;
+    //     endcase 
+    // end
 
 endmodule
