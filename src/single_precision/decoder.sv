01dac4cbe21ef235e1c36406da016c99696e4ff2
Saandiya <saandiya.mohan@gmail.com>
2024-11-11 16:23:32 -0500

    decoder and top

--- src/single_precision/decoder.sv
+++ src/single_precision/decoder.sv
@@ -1,5 +1,17 @@
 module decoder (
-
+    input logic [6:0]in,
+    output logic [1:0]out
 );
+
+always_comb begin 
+    case(in)
+    7'b0000000: begin //add
+        out = 2'd1; end
+    7'b0000001: begin //sub
+        out = 2'd2; end
+    7'b0000010: begin //mult
+        out = 2'd3; end
+    endcase
+end
 
 endmodule
--- src/single_precision/top_FPU.sv
+++ src/single_precision/top_FPU.sv
@@ -4,23 +4,25 @@ module top_FPU (
     input logic [6:0]opcode
 );
 
-logic [31:0]result_add, data2_in;
-logic [31:0]result_sub, result_mult;
+logic [31:0]data2_in;
+logic [31:0]result_sub, result_mult, result_add;
 logic overflow_mult, underflow_mult;
 logic overflow_add, underflow_add;
+logic [1:0]opcode_out;
 
 
 adder u0 (.data1(data1), .data2(data2_in), .result(result_add), .overflow(overflow_add), .underflow(underflow_add));
 multiplication u1(.data1(data1), .data2(data2), .result(result_mult), .overflow(overflow_mult), .underflow(underflow_mult));
+decoder u2(.in(opcode), .out(opcode_out));
 
 always_comb begin
-    if (opcode == 2'd1) begin
+    if (opcode_out == 2'd1) begin
         data2_in = data2;
         result = result_add; end
-    else if (opcode = 2'd2) begin
+    else if (opcode_out = 2'd2) begin
         data2_in = {~data2[31],data2[30:0]};
         result = result_add; end
-    else if (opcode = 2'd3) begin
+    else if (opcode_out = 2'd3) begin
         result = result_mult; end
 end
 
