eb702bff12838bf16e840cd800ec7aca9d75510d
Riya Chamala <chamala.riyaa@gmail.com>
2025-07-29 15:32:39 -0400

    Update fifo_driver.sv

--- uvm_tb_new/fifo_driver.sv
+++ uvm_tb_new/fifo_driver.sv
@@ -1,19 +1,26 @@
 class fifo_driver extends uvm_driver #(fifo_seq_item);
     virtual fifo_interface vif;
+
     `uvm_component_utils(fifo_driver)
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
     endfunction
 
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        if (!uvm_config_db#(virtual fifo_interface)::get(this, "", "vif", vif))
+            `uvm_fatal("NOVIF", "No virtual interface set for fifo_driver")
+    endfunction
+
     task run_phase(uvm_phase phase);
         fifo_seq_item item;
         forever begin
             seq_item_port.get_next_item(item);
+            @(posedge vif.clk);
             vif.write_en = item.write;
             vif.read_en  = item.read;
             vif.data_in  = item.data;
-            @(posedge vif.clk);
             seq_item_port.item_done();
         end
     endtask
